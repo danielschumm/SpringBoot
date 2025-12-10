3b65ea304287030daddba263751a90955a5ee13d
danielschumm <60554211+danielschumm@users.noreply.github.com>
2025-12-10 15:51:19 -0500

    init

---
+++ src/test/java/com/daniels/tickets/TicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniels.tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
