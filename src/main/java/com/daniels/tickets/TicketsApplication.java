a4ab1bd572072f10d4915f1139de90d16dfc240a
Daniel S <daniel.schumm@usan.com>
2025-12-10 20:44:46 -0500

    fromlaptop

--- src/main/java/com/daniels/tickets/TicketsApplication.java
+++ src/main/java/com/daniels/tickets/TicketsApplication.java
@@ -2,7 +2,7 @@ package com.daniels.tickets;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//hs
+//hs1
 @SpringBootApplication
 public class TicketsApplication {
 
