286aa1936d3b4b2f7b36aeb566010e37fdbcbe10
danielschumm <60554211+danielschumm@users.noreply.github.com>
2026-01-28 13:40:26 -0500

    Added postgresql functionality

--- src/main/java/com/daniels/tickets/events/Event.java
+++ src/main/java/com/daniels/tickets/events/Event.java
@@ -2,6 +2,105 @@ package com.daniels.tickets.events;
 
 import java.time.LocalDate;
 
-public record Event(int id, String name, Organizer organizer, Venue venue, LocalDate startDate, LocalDate endDate) {
+import jakarta.persistence.*;
+//import java.time.LocalDate;
 
+@Entity
+@Table(name = "events")
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String name;
+
+
+    @ManyToOne
+    @JoinColumn(name = "organizer_id", referencedColumnName = "id", nullable = false)
+    private Organizer organizer;
+
+    @ManyToOne
+    @JoinColumn(name = "venue_id", referencedColumnName = "id", nullable = false)
+    private Venue venue;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDate startDate;
+
+    @Column(name = "end_date", nullable = false)
+    private LocalDate endDate;
+
+    public Event() {}
+
+
+    public Event(int id, String name, Organizer organizer, Venue venue, LocalDate startDate, LocalDate endDate) {
+        this.id = id;
+        this.name = name;
+        this.organizer = organizer;
+        this.venue = venue;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Organizer getOrganizer() {
+        return organizer;
+    }
+
+    public void setOrganizer(Organizer organizer) {
+        this.organizer = organizer;
+    }
+
+    public Venue getVenue() {
+        return venue;
+    }
+
+    public void setVenue(Venue venue) {
+        this.venue = venue;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", organizer=" + organizer +
+                ", venue=" + venue +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                '}';
+    }
 }
--- src/main/java/com/daniels/tickets/events/Venue.java
+++ src/main/java/com/daniels/tickets/events/Venue.java
@@ -1,8 +1,86 @@
 package com.daniels.tickets.events;
 
+import jakarta.persistence.*;
 
-public record Venue(int id, String name, String street, String city, String country){
 
+@Entity
+@Table(name = "venues")
+public class Venue {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String street;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String country;
+
+    public Venue() {}
+
+    public Venue(int id, String name, String street, String city, String country) {
+        this.id = id;
+        this.name = name;
+        this.street = street;
+        this.city = city;
+        this.country = country;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "Venue{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
 }
-
-
