65791ab0950249f4f70db35719a78979ed310bbf
danielschumm <60554211+danielschumm@users.noreply.github.com>
2026-05-08 13:29:08 -0400

    Testing register endpoint on aws

--- ticket-management-api/events-service/src/main/java/com/daniels/springboot/auth/JwtController.java
+++ ticket-management-api/events-service/src/main/java/com/daniels/springboot/auth/JwtController.java
@@ -4,6 +4,9 @@ import org.springframework.security.access.method.P;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -40,8 +43,10 @@ public class JwtController {
         return jwtService.generateToken(userDetails.getUsername());
     }
     @PostMapping("/register")
-    public String register(@RequestBody AuthRequest authRequest) {
-
+    public String register(@RequestBody AuthRequest authRequest, HttpServletRequest request) {
+        System.out.println("REQUEST URI: " + request.getRequestURI());
+        System.out.println("METHOD: " + request.getMethod());
+        System.out.println("Register attempt for user: " + authRequest.getUsername());
         System.out.println("Register attempt for user: " + authRequest.getUsername());
         User newUser = new User();
         newUser.setUsername(authRequest.getUsername());
